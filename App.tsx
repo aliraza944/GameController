afb0f2558eff7e40ea934fd0824594b587796191
aliraza944 <alisabri022@gmail.com>
2022-03-07 21:42:51 +0500

    Added the basic boiler plate code

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Game Controller</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
