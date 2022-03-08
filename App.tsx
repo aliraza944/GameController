41a3d84735fd391958831092205a350f2e292572
aliraza944 <alisabri022@gmail.com>
2022-03-08 11:56:57 +0500

    fonts added and configured

--- App.tsx
+++ App.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-
+import {fonts} from './assets/Data';
 const App = () => {
   return (
     <View>
-      <Text>Game Controller</Text>
+      <Text style={styles.text}>Game Controller</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  text: {
+    fontFamily: fonts.extraBold,
+    color: 'black',
   },
 });
 
---
+++ assets/Data.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+export const fonts = {
+  bold: 'Poppins-Bold',
+  extraBold: 'Poppins-ExtraBold',
+  extraLight: 'Poppins-ExtraLight',
+  light: 'Poppins-Light',
+  medium: 'Poppins-Medium',
+  regular: 'Poppins-Regular',
+  semiBold: 'Poppins-SemiBold',
+  thin: 'Poppins-Thin',
+};
